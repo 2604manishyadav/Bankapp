b8a1e3dd12fb0c897ac2105f425e1e6e9230c4f3
2604manishyadav <97606893+2604manishyadav@users.noreply.github.com>
2025-06-20 21:13:58 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -67,7 +67,7 @@ resource "aws_security_group" "allow_user_to_connect" {
 
 resource "aws_instance" "testinstance" {
   ami             = data.aws_ami.os_image.id
-  instance_type   = var.my_enviroment == "prd" ? "t2.medium" : "t2.micro"
+  instance_type   = var.instance_type
   key_name        = aws_key_pair.deployer.key_name
   security_groups = [aws_security_group.allow_user_to_connect.name]
   
